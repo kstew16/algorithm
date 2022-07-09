d11a3141aaf62b8874aeded5154ce19a996f723b
Eunwoo Tae <37829895+kstew16@users.noreply.github.com>
2022-07-09 23:31:58 +0900

    220709 combinations 14391 fail
    그냥 무력하게 당함

---
+++ boj_kot/src/main/kotlin/14391.kt
@@ -0,0 +1,105 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+fun main(){
+    infix fun Int.on(i:Int) = this or (1 shl i)
+    infix fun Int.off(i:Int) = this and (1 shl i).inv()
+    infix fun Int.chk(i:Int) = this and (1 shl i)>=1
+
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val (col, row) = br.readLine().split(" ").map{it.toInt()}
+    val maxDepth = col*row
+    var stringBuilder = ""
+    for(i in 0 until col){
+        stringBuilder += br.readLine()
+    }
+    val tableArray = (stringBuilder.split("").subList(1,maxDepth+1)).map { it.toInt() }.toIntArray()
+    var indexVisited = 0
+    val stack = mutableListOf<Int>()
+    var maxSum = 0
+
+    val NOTHING = 0
+    val HORIZONTAL = 1
+    val VERTICAL = 2
+
+    fun visit(target:Int, direction:Int, lastSum:Int){
+        // 1. 스택에 뭔가 들어 있으면 끊고 갈 수 있음
+        if(stack.size>0){
+            // 끊고 가는 경우 어디로든지 갈 수 있는데, 스택이 sum 으로 치환돼서 들어감.
+            var localSum = lastSum + stack.joinToString("").toInt()
+            if(!(indexVisited chk target+1) && target + 1 < maxDepth && (target + 1) % row != 0 ){
+                // 오른쪽 블럭이 방문되지 않았고, 존재할 때
+                // 스택 백업
+                var tmp = mutableListOf<Int>()
+                stack.forEach {
+                    tmp.add(it)
+                }
+                stack.clear()
+                stack.add(tableArray[target])
+                indexVisited = indexVisited on target
+                visit(target+1,HORIZONTAL,localSum)
+                stack.removeAt(stack.size - 1)
+                indexVisited = indexVisited off target
+                tmp.forEach{
+                    stack.add(it)
+                }
+            }
+            if(!(indexVisited chk target+row) && target + row < maxDepth){
+                var tmp = mutableListOf<Int>()
+                stack.forEach {
+                    tmp.add(it)
+                }
+                stack.clear()
+                stack.add(tableArray[target])
+                indexVisited = indexVisited on target
+                visit(target+row,VERTICAL,localSum)
+                stack.removeAt(stack.size - 1)
+                indexVisited = indexVisited off target
+                tmp.forEach{
+                    stack.add(it)
+                }
+            }
+        }
+
+        stack.add(tableArray[target])
+        indexVisited = indexVisited on target
+        if(indexVisited == ((1 shl maxDepth) - 1)){
+            // 모든 블럭이 방문 상태이면 마무리하고 합 계산
+            var localSum = lastSum + stack.joinToString("").toInt()
+            maxSum = if(maxSum<localSum) localSum else maxSum
+            //return
+        }
+
+        // 2. 확장 블럭 선정 - 방향을 계승하고, 블럭 유효성을 검사
+        // 블럭을 끊지 않고 (스택을 sum 으로 치환하지 않고) 진행하는 경우에는 전 블럭이 진행하던 방향이 중요
+        when(direction){
+            NOTHING ->{
+                // 새로운 블럭이 시작된 경우 어디로든 확장 가능
+                if(!(indexVisited chk target+1) && target + 1 < maxDepth && (target + 1) % row != 0 ){
+                    // 오른쪽 블럭이 방문되지 않았고, 존재하며, 줄을 넘어간 블럭이 아닐 때
+                    visit(target+1,HORIZONTAL,lastSum)
+                }
+                if(!(indexVisited chk target+row) && target + row < maxDepth){
+                    visit(target+row,VERTICAL,lastSum)
+                }
+            }
+            HORIZONTAL->{
+                // 가로로 확장하던 것을 이어갈 때
+                if(!(indexVisited chk target+1) && target + 1 < maxDepth && (target + 1) % row != 0 ){
+                    visit(target+1,HORIZONTAL,lastSum)
+                }
+                // 세로로 가려면 끊고 가야 가능
+            }
+            VERTICAL->{
+                if(!(indexVisited chk target+row) && target + row < maxDepth){
+                    visit(target+row,VERTICAL,lastSum)
+                }
+            }
+        }
+
+        stack.removeAt(stack.size - 1)
+        indexVisited = indexVisited off target
+    }
+
+    visit(0,NOTHING,0)
+    println(maxSum)
+}
--- boj_kot/src/main/kotlin/note.kt
+++ boj_kot/src/main/kotlin/note.kt
@@ -1,44 +1,112 @@
-fun main(){
-    val br = System.`in`.bufferedReader()
-    val bw = System.`out`.bufferedWriter()
-    do{
-        val n = br.readLine().toInt()
-        val input = br.readLine().split(" ").map { it.toInt() } as ArrayList<Int>
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 
-        var x = n+1
-        for (i in n-2 downTo 0){
-            if(input[i]>input[i+1]){
-                x = i
-                break
+fun main(){
+    infix fun Int.on(i:Int) = this or (1 shl i)
+    infix fun Int.off(i:Int) = this and (1 shl i).inv()
+    infix fun Int.chk(i:Int) = this and (1 shl i)>=1
+
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val (col, row) = br.readLine().split(" ").map{it.toInt()}
+    val maxDepth = col*row
+    """val tableCol = Array(col){
+        br.readLine().split("").subList(1,row+1).map {
+            it.toInt()
+        }.toIntArray()
+    }
+    """
+    var stringBuilder = ""
+    for(i in 0 until col){
+        stringBuilder += br.readLine()
+    }
+    val tableArray = (stringBuilder.split("").subList(1,maxDepth+1)).map { it.toInt() }.toIntArray()
+    var indexVisited = 0
+    val stack = mutableListOf<Int>()
+    var sum = 0
+    var maxSum = 0
+
+    """
+        depth 몇 개의 블럭을 사용했는가
+        target 어떤 블럭을 탐색할 것인가
+        cut 저번에 쓴 블럭을 자르고 들어왔는가 (새로운 블럭 시작됨)
+        vertical 쓰던 블럭이 세로 블럭인가
+    """
+    fun dfs(depth: Int, target:Int, cut:Boolean, vertical:Boolean){
+        if(cut || depth == maxDepth){
+            // 조각을 자르거나 조각을 다 쓰면 해당 조각의 숫자들을 합으로 만들기
+            val piece = stack.joinToString("").toInt()
+            stack.clear()
+            sum += piece
+            if(depth == maxDepth){
+                maxSum = if(maxSum<sum)sum else maxSum
+                sum = 0
+                return
             }
         }
-        // for 문 다 돌았는데도  x가 n+1이면 주어진 순열은 첫 순열, 이전 순열 없으므로 -1 출력
-        if(x == n+1) {
-            bw.write("-1")
-            bw.write("\n")
-        }
-        else{
-            // 이제 x 오른쪽에서 자신보다 작은 수 중 가장 큰 수 모셔오기
-            var successor = 0
-            var y = 0
-            for(i in x+1 until n){
-                if(input[x] > input[i] && input[i] > successor ){
-                    successor = input[i]
-                    y = i
+        for(i in target until maxDepth){
+            if(cut){
+                // 쓰던 블럭을 자르고 들어온 경우 세로와 가로 모두 확장 가능
+                if(!(indexVisited chk i+1) && i+1<maxDepth){
+                    // 가로 확장 가능한 경우 가로 확장
+                    indexVisited = indexVisited on (i+1)
+                    stack.add(tableArray[i+1])
+                    if(i%row!=row-1) {
+                        // 가로 끝 블럭이 아니어야 가로확장 가능
+                        dfs(depth+1,i+1,cut = false,vertical = false)
+                    }
+                    dfs(depth+1,i+1,cut = true, vertical = false)
+                    stack.removeAt(stack.size-1)
+                    indexVisited = indexVisited off (i+1)
+                }
+                if(!(indexVisited chk i+row) && i+row<maxDepth){
+                    // 세로 확장 가능한 경우 가로 확장
+                    indexVisited =indexVisited on (i+row)
+                    stack.add(tableArray[i+row])
+                    if(i<(row*(col-1))){
+                        dfs(depth+1,i+row,cut = false,vertical = true)
+                    }
+                    dfs(depth+1,i+row,cut = true, vertical = true)
+                    stack.removeAt(stack.size-1)
+                    indexVisited = indexVisited off (i+row)
                 }
             }
-            // 오른쪽에 자신보다 작은 수를 찾으면 successor 는 0이 아님
-            // 찾지 못했으면 그대로 x 오른쪽을 역순정렬
-            if(successor != 0){
-                input[y] = input[x]
-                input[x] = successor
+            else{
+                //자르지 않고 들어온 경우 쓰던 블럭 방향대로만 사용 가능
+                if(vertical){
+                    if(!(indexVisited chk i+row) && i+row<maxDepth){
+                        // 세로 확장 가능한 경우 가로 확장
+                        indexVisited = indexVisited on (i+row)
+                        stack.add(tableArray[i+row])
+                        if(i<(row*(col-1))){
+                            dfs(depth+1,i+row,cut = false,vertical = true)
+                        }
+                        dfs(depth+1,i+row,cut = true, vertical = true)
+                        stack.removeAt(stack.size-1)
+                        indexVisited =indexVisited off (i+row)
+                    }
+                }
+                else{
+                    // 가로로 확장하던 경우
+                    if(!(indexVisited chk i+1) && i+1<maxDepth){
+                        indexVisited = indexVisited on (i+1)
+                        stack.add(tableArray[i+1])
+                        if(i%row!=row-1) {
+                            // 가로 끝 블럭이 아니어야 가로확장 가능
+                            dfs(depth+1,i+1,cut = false,vertical = false)
+                        }
+                        dfs(depth+1,i+1,cut = true, vertical = false)
+
+                        stack.removeAt(stack.size-1)
+                        indexVisited = indexVisited off (i+1)
+                    }
+                }
             }
-            // x 오른쪽의 서브리스트를 역순 정렬하여 출력
-            var ans = input.subList(0,x+1) + (input.subList(x+1,input.size)).sortedDescending()
-            bw.write(ans.joinToString(" "))
-            bw.write("\n")
         }
-    }while(n!=-1)
-    bw.close()
-    br.close()
+    }
+    stack.add(tableArray[0]) // 0 부터 시작하는 탐색
+    dfs(0,0,cut=true,vertical=true)
+    dfs(0,0,cut=false,vertical=true)
+    dfs(0,0,cut=false,vertical=false)
 }
