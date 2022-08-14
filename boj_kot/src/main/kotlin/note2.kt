802d4405eb760ab490b4360b08916227ed6c6292
Eunwoo Tae <37829895+kstew16@users.noreply.github.com>
2022-08-14 19:01:19 +0900

    220814 BF
    팁팁팁 upgraded

--- boj_kot/src/main/kotlin/note2.kt
+++ boj_kot/src/main/kotlin/note2.kt
@@ -4,10 +4,44 @@ import java.util.LinkedList
 import java.util.StringTokenizer
 
 private fun main(){
-    println(100000)
-    for(i in 0 until 100000){
-        //val offset = if(i%2==1) 0 else {if((i/2)%2==0)1 else -1}
-        val offset = if(i<100)1 else 0
-        print("${i*5+offset} ")
+    fun nextTurn(pos:Int,dice:Int): IntArray {
+        var nextPos = pos+dice
+        var result: IntArray
+        when{
+            (pos in 50..53) -> {
+                result = if(nextPos>53) nextTurn(197,nextPos-53-1)
+                else intArrayOf(3*(nextPos-50) + 10,nextPos)
+            }
+            (pos in 100..102) -> {
+                result = if(nextPos>102) nextTurn(197,nextPos-102-1)
+                else intArrayOf(2*(nextPos-100) + 20,nextPos)
+            }
+            (pos in 150..153) -> {
+                //153 에서 26 152에서 27 151에서 28
+                result = if(nextPos>153) nextTurn(197,nextPos-153-1)
+                else intArrayOf((-1)*(nextPos-150) + 29,nextPos)
+            }
+            (pos in 197..199) -> {
+                // 197->25 198->30 199->35 200->40
+                result = if(nextPos>200) intArrayOf(0,300)
+                else intArrayOf((200-nextPos)*(-5) + 40,nextPos)
+            }
+            else ->{
+                nextPos = pos + 10*dice
+                result = if(nextPos>200) intArrayOf(0,300)
+                else intArrayOf(nextPos/5,nextPos)
+            }
+        }
+        return result
+    }
+    for(i in 0 until 20){
+        for(j in 1..5){
+            println("$i : " + nextTurn(i*10,j).joinToString(" "))
+        }
+    }
+    for(i in intArrayOf(50,51,52,53,100,101,102,150,151,152,153,197,198,199)){
+        for(j in 1..5){
+            println("$i : " + nextTurn(i,j).joinToString(" "))
+        }
     }
 }
