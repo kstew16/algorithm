1ecd72b6611190f95af394ddb9134265653cef87
Nunu <kstew16@naver.com>
2022-08-15 23:27:36 +0900

    Update 16987.kt

--- boj_kot/src/main/kotlin/16987.kt
+++ boj_kot/src/main/kotlin/16987.kt
@@ -1,7 +1,7 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.StringTokenizer
-
+// 1시간 정도 걸림, 팁에도 적어놨지만 전역변수를 재귀 탈출시에 원상복구하지 않고 재귀 제한 도달시에 초기화했다가 틀림
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
     val n = readLine().toInt()
     val eggs = Array(n){
@@ -15,7 +15,6 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
     fun hitDFS(depth:Int){
         if(depth == n){
             // 가장 최근에 든 계란이 가장 오른쪽 계란일 경우 과정을 종료한다
-            var temp = maxBroken
             maxBroken = brokenEgg.coerceAtLeast(maxBroken)
         }else{
             if(eggs[depth][0]<=0){
@@ -30,13 +29,11 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
                         val (targetHP,targetPower) = eggs[i]
                         if(targetHP>0){
                             isHit = true
-                            //stackForTest[depth] = i
                             eggs[i][0] = targetHP - hittingPower
                             eggs[depth][0] = hittingHP - targetPower
                             if(targetHP<=hittingPower) brokenEgg+=1
                             if(hittingHP<=targetPower) brokenEgg+=1
                             hitDFS(depth+1)
-                            //stackForTest[depth] = -1
                             eggs[i][0] = targetHP
                             eggs[depth][0] = hittingHP
                             if(targetHP<=hittingPower) brokenEgg-=1
