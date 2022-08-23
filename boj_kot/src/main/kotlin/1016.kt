193ba15ee9b9a5450dc50460a6a2b41b481303b0
Nunu <kstew16@naver.com>
2022-08-24 02:33:34 +0900

    220823 eratosthenes_long
    eratosthenes with big number

--- boj_kot/src/main/kotlin/1016.kt
+++ boj_kot/src/main/kotlin/1016.kt
@@ -1,7 +1,7 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import kotlin.math.sqrt
-
+// 제틴 진우한테 에라토스테네스라는 힌트를 먼저 받았기도 하고, 시간도 오래걸렸음
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
     val (min, max) = readLine().split(" ").map{it.toLong()}
     fun eratosthenes(s:Int,e:Int):MutableList<Int>{
@@ -38,21 +38,20 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
 
     // 제곱수 s 에 대해, min 이상의 s 의 배수부터 max 이하의 s 의 배수를 제곱수에서 체크 해제
     for(i in squareNum.indices){
-        val divisor = squareNum[i]
-        var dividendMaxLong = ((max-max%divisor) - min)
-        var dividendMinLong = if(divisor<min)(divisor-min%divisor)else(min-min%divisor)
-        if(dividendMaxLong<0  || dividendMinLong<0) {
-            continue
-        }
-        var dividendMax = dividendMaxLong.toInt()
-        var dividendMin = dividendMinLong.toInt()
+        val s = squareNum[i]
+        var dividendUnderMax = (max-max%s)
+        var dividendUpperMin = if(min%s == 0L)min else (min-min%s + s)
+        // min 이상이고 max 이하인 s 의 배수가 없는 경우
+        if(dividendUnderMax<dividendUpperMin) continue
 
-        var j = dividendMax
-        while(j>=dividendMin){
+        // min 이상의 s 의 배수가 min 으로부터 얼마나 떨어져 있는가
+        var startOffset = (dividendUpperMin - min).toInt()
 
+        var j = startOffset
+        while(j in isTarget.indices){
             isTarget[j] = false
-            if(divisor>j.toLong()) break
-            j -= divisor.toInt()
+            // divisor 에 의한 오버플로우 주의
+            j += s.toInt()
         }
     }
     print(isTarget.count{it})
