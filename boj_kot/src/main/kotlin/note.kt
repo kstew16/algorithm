ec02074719825d62107edc08e16505f77f6abb60
Nunu <kstew16@naver.com>
2022-08-31 00:31:32 +0900

    220831 mutableSet
    hashSet보다 mutableSet 이 빠르고 절약되더라

---
+++ boj_kot/src/main/kotlin/note.kt
@@ -0,0 +1,12 @@
+fun main(){
+    println(30000)
+    for(i in 0 until 30000){
+        print("$i ")
+    }
+    println()
+    println(30000)
+    for(i in 0 until 30000){
+        print("${2*i} ")
+    }
+    println()
+}
