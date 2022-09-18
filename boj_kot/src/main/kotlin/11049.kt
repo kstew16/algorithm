422436bceae59c1695e48222afbedc2858ba8e07
Eunwoo Tae <37829895+kstew16@users.noreply.github.com>
2022-09-18 19:54:29 +0900

    220918 dphelpme

--- boj_kot/src/main/kotlin/11049.kt
+++ boj_kot/src/main/kotlin/11049.kt
@@ -1,7 +1,7 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.StringTokenizer
-
+// 점화식이 완벽하지 않았음. 답코드 보고서 점화식 틀린 거 알아챘음. 수업까지 들었는데 ㅜ
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
     val n = readLine().toInt()
     val mat = Array(n){
@@ -9,15 +9,15 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
         IntArray(2){st.nextToken().toInt()}
     }
     // dp[a][b] ->  a 번째 행렬부터 b 번째 행렬까지 곱하는 최소 연산횟수
-    //val dp = Array(n){IntArray(n){Int.MAX_VALUE}}.apply{for(i in 0 until n)this[i][i]=0}
     val dp = Array(n){IntArray(n){0}}
-    // dp[a][b] = min(dp[a][b-1] + mat[a][0]*mat[b-1][1]*mat[b][1] ,dp[a+1][b] + mat[a][0]*mat[a+1][0]*mat[b][1])
+    // dp[a][b] = for(k in 0 until i) min(dp[a][a+k]+dp[a+k+1][b]+mat[a][0]*mat[a+k][1]*mat[b][1]
     for(i in 1 until n){
         for(j in 0 until n-i){
-            dp[j][j+i] =  kotlin.math.min(
-                dp[j][j+i-1] + mat[j][0]*mat[j+i-1][1]*mat[j+i][1],
-                dp[j+1][j+i] + mat[j][0]*mat[j+1][0]*mat[j+i][1]
-            )
+            var optimum = Int.MAX_VALUE
+            for(k in 0 until i){
+                optimum = (dp[j][j+k] + dp[j+k+1][j+i] + mat[j][0]*mat[j+k][1]*mat[j+i][1] ).coerceAtMost(optimum)
+            }
+            dp[j][j+i] = optimum
         }
     }
     print(dp[0][n-1])
---
+++ boj_kot/src/main/kotlin/17069.kt
@@ -0,0 +1,47 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+import kotlin.contracts.contract
+
+private const val HORIZONTAL = 0
+private const val VERTICAL = 1
+private const val DIAGONAL = 2
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
+    val n = readLine().toInt()
+    val field = Array(n){
+        val st = StringTokenizer(readLine())
+        IntArray(n){st.nextToken().toInt()}
+    }
+
+    val ways = Array(n){Array(n){LongArray(3){0}}}.apply{this[0][1][HORIZONTAL] = 1}
+    (0 until n).forEach { j->
+        (0 until n).forEach { i->
+            if(field[j][i]==0){
+                // Horizontal
+                if(i-1 in 0 until n){
+                    // 좌 Horizontal
+                    ways[j][i][HORIZONTAL] += ways[j][i-1][HORIZONTAL]
+                    // 좌 Diagonal
+                    if(j-1 in 0 until n)
+                        ways[j][i][HORIZONTAL] += ways[j][i-1][DIAGONAL]
+                }
+                // Vertical
+                if(j-1 in 0 until n){
+                    // 상 Vertical
+                    ways[j][i][VERTICAL] += ways[j-1][i][VERTICAL]
+                    // 상 Diagonal
+                    if(i-1 in 0 until n)
+                        ways[j][i][VERTICAL] += ways[j-1][i][DIAGONAL]
+                }
+                // Diagonal
+                if(j-1 in 0 until n && i-1 in 0 until n){
+                    // 사 Diagonal
+                    if(field[j-1][i]+field[j][i-1]==0)
+                        ways[j][i][DIAGONAL] += ways[j-1][i-1][DIAGONAL] + ways[j-1][i-1][VERTICAL] + ways[j-1][i-1][HORIZONTAL]
+                }
+            }
+        }
+    }
+
+    print(ways[n-1][n-1].sum())
+}
