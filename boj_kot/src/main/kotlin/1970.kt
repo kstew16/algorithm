f2c6a92186d3621a333b0b246785d7046cf0c209
Nunu <kstew16@naver.com>
2022-10-07 01:32:27 +0900

    221007 1970 idea

---
+++ boj_kot/src/main/kotlin/1970.kt
@@ -0,0 +1,9 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+// 선 하나를 긋는 것은 쪼개진 두 개의 문제를 낳는다. 모든 악수 대상에 대해서 차례대로 부분 문제의 최댓값을 취하는 방식으로 하면 될듯 일단 T-B
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
+
+
+}
