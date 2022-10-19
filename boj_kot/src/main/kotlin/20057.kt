59a9eb3ff7c4c788f05316785c98d4c843b266e1
Nunu <kstew16@naver.com>
2022-10-19 18:10:42 +0900

    221019 implementation

--- boj_kot/src/main/kotlin/20057.kt
+++ boj_kot/src/main/kotlin/20057.kt
@@ -12,32 +12,57 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
     // 위 오른 아래 왼 순으로 0,1,2,3
     val dy = intArrayOf(-1,0,1,0)
     val dx = intArrayOf(0,1,0,-1)
-    fun Int.turnRight() = (this+1)%4
-    fun Int.turnLeft() = (this+3)%4
 
     var sandBlownOut = 0
 
-    class Tornado(var py:Int, var px:Int){
-        fun move(d:Int){
-
+    fun Int.left() = (this+3)%4
+    fun Int.right() = (this+1)%4
+    class Tornado(var py:Int, var px:Int, var d:Int){
+        fun move():Boolean{
+            val ny = dy[d] + py ; val nx = dx[d] + px
+            if(ny !in 0 until n || nx !in 0 until n) return false
+            py = ny; px = nx
+            blow()
+            return true
         }
-        fun blow(d:Int){
+        fun blow(){
             // 범위 밖으로 나가는 모래 측정해야함
+            val currentSand = field[py][px]
+            var sandUsed = 0
+            val right = d.right()
+            // val left = d.left()
+            // 1퍼 1퍼 7퍼 7퍼 10퍼 10퍼 2퍼 2퍼 5퍼 나머지
+            val dSide = intArrayOf(1,-1,1,-1,1,-1,2,-2,0,0)
+            val dDirectional = intArrayOf(-1,-1,0,0,1,1,0,0,2,1)
+            val blowingRatio = intArrayOf(1,1,7,7,10,10,2,2,5)
+            for(i in 0 until 9){
+                val ny = py + dDirectional[i]*dy[d] +dSide[i]*dy[right]
+                val nx = px + dDirectional[i]*dx[d] +dSide[i]*dx[right]
+                val sandBlowing = (blowingRatio[i]*currentSand)/100
+                sandUsed += sandBlowing
+                if(ny in 0 until n && nx in 0 until n) field[ny][nx]+=sandBlowing
+                else sandBlownOut += sandBlowing
+            }
+            val ny = py + dy[d]
+            val nx = px + dx[d]
+            val sandBlowing = currentSand-sandUsed
+            if(ny in 0 until n && nx in 0 until n) field[ny][nx]+=sandBlowing
+            else sandBlownOut += sandBlowing
+            field[py][px] = 0
         }
+        fun turnLeft(){this.d = this.d.left()}
     }
 
-    var moveCount = 0
-    val limit = n*n-1
     var movingDistance = 1 // 초기 이동량은 1
-    var movingDirection = 3 // 초기 운동방향은 좌측
-    val tornado = Tornado(n/2,n/2)
-    while(moveCount<limit){
+    val tornado = Tornado(n/2,n/2,3)
+    var active = true
+    while(active){
         for(i in 0 until 2){
             repeat(movingDistance){
-                tornado.move(movingDirection)
-                tornado.blow(movingDirection)
+                active = tornado.move()
             }
-            movingDirection = movingDirection.turnLeft()
+            if(!active) break
+            tornado.turnLeft()
         }
         movingDistance+=1
     }
