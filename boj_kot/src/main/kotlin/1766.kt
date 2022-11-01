a86627c1b8260b167b21d8d26a6fb2bf8a847536
Nunu <kstew16@naver.com>
2022-11-01 16:02:45 +0900

    221101 1766 shorter

--- boj_kot/src/main/kotlin/1766.kt
+++ boj_kot/src/main/kotlin/1766.kt
@@ -14,20 +14,11 @@ fun main() : Unit = with(BufferedReader(InputStreamReader(System.`in`))){
         accept[a].add(b)
         conditionCount[b] +=1
     }
-    val pq = PriorityQueue<Int>{a,b ->
-        when{
-            a<b -> -1
-            else -> 1
-        }
-    }
+    val pq = PriorityQueue<Int>().apply{for(i in 1..n) if(conditionCount[i]==0) this.add(i)}
     val sb = java.lang.StringBuilder("")
-    val visited = BooleanArray(n+1)
-    for(i in 1..n) if(conditionCount[i]==0) pq.add(i)
     while(pq.isNotEmpty()){
         val cur = pq.poll()
-        if(visited[cur]) continue
         sb.append(" $cur")
-        visited[cur] = true
         accept[cur].forEach {
             if(--conditionCount[it]==0) pq.add(it)
         }
