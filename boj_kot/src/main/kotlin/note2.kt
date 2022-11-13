b194f6a695434eef11e737281b458f44ad3576c2
Eunwoo Tae <37829895+kstew16@users.noreply.github.com>
2022-11-13 20:01:15 +0900

    221113 binary search 2

--- boj_kot/src/main/kotlin/note2.kt
+++ boj_kot/src/main/kotlin/note2.kt
@@ -4,47 +4,10 @@ import java.util.StringTokenizer
 import kotlin.math.abs
 
 fun main():Unit = with(BufferedReader(InputStreamReader(System.`in`))){
-    val n = readLine().toInt()
-    val st = StringTokenizer(readLine())
-    val arr = IntArray(n){st.nextToken().toInt()}
-    var a = 0
-    var b = 0
-    var minSum = Int.MAX_VALUE
-    data class Candidate(val index:Int,val ph:Int)
-    fun getCandidate(lo:Int,hi:Int){
-        val mid = (lo+hi)/2
+    println(5000)
+    for(i in 0 until 4999){
+        print((-1000000000L + (2000000000/5000).toLong()*i))
+        print(" ")
     }
-    for(i in 0 until n-1){
-        val cur = arr[i]
-        var lo = i+1
-        var hi = n-1
-        var mid = (lo+hi)/2
-        var sumHi = abs(cur+arr[hi])
-        var sumLo = abs(cur+arr[lo])
-        var sumMid = abs(cur+arr[mid])
-        while(lo+1<hi){
-            if(abs(cur+arr[hi])>abs(cur+arr[lo])) hi = mid
-            else lo = mid
-            mid = (lo+hi)/2
-            sumHi = abs(cur+arr[hi])
-            sumLo = abs(cur+arr[lo])
-            sumMid = abs(cur+arr[mid])
-        }
-        val candidate:Int
-        val candidateVal:Int
-        if(sumHi>sumLo){
-            candidate = sumLo
-            candidateVal = arr[lo]
-        }
-        else{
-            candidate = sumHi
-            candidateVal = arr[hi]
-        }
-        if(candidate<minSum){
-            minSum = candidate
-            a = cur
-            b = candidateVal
-        }
-    }
-    print("$a $b")
+    println(1000000000)
 }
