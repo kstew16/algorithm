5ad99371e76a6a9758fbd4640149e35c3e98df81
Eunwoo Tae <37829895+kstew16@users.noreply.github.com>
2022-11-19 23:56:15 +0900

    221119 LCS trace

--- boj_kot/src/main/kotlin/9252.kt
+++ boj_kot/src/main/kotlin/9252.kt
@@ -8,45 +8,44 @@ fun main():Unit = with(BufferedReader(InputStreamReader(System.`in`))){
     val sizeB = arr2.size
     // a 에서 문자열 1 끝까지, b 에서 문자열 2 끝까지 사용했을 때 얻을 수 있는 LCS 의 길이
     val dp = Array(sizeA+1){IntArray(sizeB+1){-1} }
-    // 그 LCS 를 만드는 다음 문자가 dp 어디에서 온 건지
     val next = Array(sizeA+1){Array(sizeB+1){Pair(-1,-1)} }
-    fun getLCS(a:Int,b:Int):Pair<Int, Pair<Int,Int>>{
+    fun getLCS(a:Int,b:Int):Int{
         var maxLen = 0
-        var usingCharIndex = Pair(-1,-1)
-        if(a==sizeA || b==sizeB) return Pair(maxLen,usingCharIndex)
-        if(dp[a][b]!=-1) return Pair(dp[a][b],next[a][b])
+        if(a==sizeA || b==sizeB) return maxLen
+        if(dp[a][b]!=-1) return dp[a][b]
         if(arr1[a]==arr2[b]){
             with(getLCS(a+1,b+1)){
-                maxLen = this.first+1
-                next[a][b] = this.second
+                maxLen = this+1
+                next[a][b] = Pair(a+1,b+1)
             }
         }
         with(getLCS(a,b+1)){
-            if(this.first>maxLen){
-                maxLen = this.first
-                next[a][b] = this.second
+            if(this>maxLen){
+                maxLen = this
+                next[a][b] = Pair(a,b+1)
             }
         }
         with(getLCS(a+1,b)){
-            if(this.first>maxLen){
-                maxLen = this.first
-                next[a][b] = this.second
+            if(this>maxLen){
+                maxLen = this
+                next[a][b] = Pair(a+1,b)
             }
         }
         dp[a][b] = maxLen
-        //next[a][b] = usingCharIndex
-        return Pair(maxLen,next[a][b])
+        return maxLen
     }
     with(getLCS(0,0)){
-        println(this.first)
-        if(this.first!=0){
-            var i = this.second
+        println(this)
+        if(this!=0){
+            var last = Pair(0,0)
+            var i = next[0][0]
             var sb = StringBuilder("")
             while(i.first!=-1 && i.second!=-1){
-                if(arr1[i.first]==arr2[i.second]) sb.append(arr1[i.first])
+                if(last.first+1==i.first && last.second+1==i.second) sb.append(arr1[last.first])
+                last = i
                 i = next[i.first][i.second]
             }
-            print(sb)
+            println(sb)
         }
     }
 }
