84e37b2846b8748fa136b2f93350c1a16d1156c6
Nunu <kstew16@naver.com>
2022-11-24 00:22:24 +0900

    221123 LCS2 BU

--- boj_kot/src/main/kotlin/9252_BU.kt
+++ boj_kot/src/main/kotlin/9252_BU.kt
@@ -2,43 +2,43 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-
+// Top down 으로 풀려고 몸비틀다가 실패해서 10번은 틀린듯 아니 내 예제에서는 400ms 못넘어가던데 더 좋은 예제가 뭘까
+// 시간제한이 빡빡하면 그냥 BU를 쓰자...
 fun main():Unit = with(BufferedReader(InputStreamReader(System.`in`))){
     val arr1 = readLine().toCharArray()
     val arr2 = readLine().toCharArray()
     val sizeA = arr1.size
     val sizeB = arr2.size
-    // 문자열 1 을 a 까지, 문자열 2 를 b 까지 사용했을 때 얻을 수 있는 LCS 의 길이
+    // 문자열 1 을 a 번째 문자 까지, 문자열 2 를 b 번째 문자 까지 사용했을 때 얻을 수 있는 LCS 의 길이
 
-    val dp = Array(sizeA){IntArray(sizeB){0} }
-    for(a in 1 until sizeA){
-        for(b in 1 until sizeB){
+    val dp = Array(sizeA+1){IntArray(sizeB+1){0} }
+    for(a in 1 .. sizeA){
+        for(b in 1 .. sizeB){
             if(arr1[a-1]==arr2[b-1]){
                 dp[a][b] = dp[a-1][b-1] + 1
-            }else dp[a][b] = kotlin.math.max(dp[a-1][b],dp[a][b-1])
+            }else dp[a][b] = kotlin.math.max(dp[a-1][b], dp[a][b-1])
         }
     }
-
-
 
     val bw = BufferedWriter(OutputStreamWriter(System.`out`))
-    val ans = dp[sizeA-1][sizeB-1]
+    var x = sizeA
+    var y = sizeB
+    val ans = dp[x][y]
+
     bw.write("$ans\n")
-    var count = ans
-    var x = sizeA-1
-    var y = sizeB-1
-    val ansArr = CharArray(ans)
-    while(count>0){
-        val cur = dp[x][y]
-        if(x>0 && dp[x-1][y] == cur) x-=1
-        else if(y>0 && dp[x][y-1] == cur) y-=1
-        else {
-            ansArr[--count] = arr1[x]
-            x-=1
-            y-=1
+    var ansStr = ""
+    while(dp[x][y]!=0){
+        when (dp[x][y]) {
+            dp[x-1][y] -> x--
+            dp[x][y-1] -> y--
+            else -> {
+                ansStr = arr1[x-1] + ansStr
+                x--
+                y--
+            }
         }
     }
-    bw.write(ansArr)
+    bw.write(ansStr)
     bw.flush()
     bw.close()
     close()
