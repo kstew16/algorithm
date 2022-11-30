ec32c9de30584459abc560cfad75156e34f87033
Nunu <kstew16@naver.com>
2022-12-01 00:45:51 +0900

    221201 Pow Matrix

--- boj_kot/src/main/kotlin/12849.kt
+++ boj_kot/src/main/kotlin/12849.kt
@@ -1,37 +1,40 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
-
+// 12850 풀이 후 업데이트 완료
 fun main(){
     val n = BufferedReader(InputStreamReader(System.`in`)).readLine().toInt()
     //0 정과 1 전산관 2 미래관 3 신앙관 4 진리관 5 한경직기념관 6 학생회관 7 형남공학관
-    data class Road(val s:Int,val e:Int)
-    val roads = arrayOf(
-        Road(0,1),
-        Road(0,2),
-        Road(1,2),
-        Road(1,3),
-        Road(2,3),
-        Road(2,5),
-        Road(3,4),
-        Road(3,5),
-        Road(4,5),
-        Road(4,6),
-        Road(5,7),
-        Road(6,7)
+    val kinds = arrayOf(
+        longArrayOf(0,1,1,0,0,0,0,0),
+        longArrayOf(1,0,1,1,0,0,0,0),
+        longArrayOf(1,1,0,1,0,1,0,0),
+        longArrayOf(0,1,1,0,1,1,0,0),
+        longArrayOf(0,0,0,1,0,1,1,0),
+        longArrayOf(0,0,1,1,1,0,0,1),
+        longArrayOf(0,0,0,0,1,0,0,1),
+        longArrayOf(0,0,0,0,0,1,1,0)
     )
-    //val record = Array(8){ IntArray(n+2) }
-    // dp[i][j] i%2 분 후에 j 번 건물에 머물고 있을 수 있는 경우의 수 어라? 규칙성 있나
-    val dp = Array(2){ IntArray(8) }.apply { this[0][0] = 1 }
-    var t = 0
-    while(t<n){
-        t++
-        val tIndex = t%2
-        val tLastIndex = if(tIndex==1) 0 else 1
-        for(i in 0 until 8) dp[tIndex][i] = 0
-        roads.forEach {
-            dp[tIndex][it.e] = (dp[tIndex][it.e]+dp[tLastIndex][it.s])%1000000007
-            dp[tIndex][it.s] = (dp[tIndex][it.s]+dp[tLastIndex][it.e])%1000000007
+    fun matrixMultiply(a:Array<LongArray>,b:Array<LongArray>):Array<LongArray>{
+        val remainder = 1000000007L
+        val ans = Array(8){y->
+            LongArray(8){x->
+                var sum = 0L
+                for(i in 0 until 8){
+                    sum = (sum + (b[i][x]*a[y][i])%remainder)%remainder
+                }
+                sum
+            }
         }
+        return ans
     }
-    print(dp[n%2][0])
+    val memo = HashMap<Int,Array<LongArray>>().apply { this[1] = kinds }
+    fun powMatrix(matrix:Array<LongArray>,pow:Int):Array<LongArray>{
+        if(memo.containsKey(pow)) return memo[pow]!!
+        val sqrtMatrix = powMatrix(matrix,pow/2)
+        var rArr = matrixMultiply(sqrtMatrix,sqrtMatrix)
+        if(pow%2==1) rArr = matrixMultiply(rArr,powMatrix(matrix,1))
+        memo[pow] = rArr
+        return rArr
+    }
+    print(powMatrix(kinds,n)[0][0])
 }
