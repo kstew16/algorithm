0af784c737837e46f4faccedf5b3f4ea78557888
Nunu <kstew16@naver.com>
2023-05-11 18:28:55 +0900

    DFS,Hash (33분,1트)

--- programmers/src/main/kotlin/여행경로.kt
+++ programmers/src/main/kotlin/여행경로.kt
@@ -1,20 +1,43 @@
 fun main(){
     class Solution {
         fun solution(tickets: Array<Array<String>>): Array<String> {
-            var answer = arrayOf<String>()
+            var path = ArrayList<Int>()
+            val answer = ArrayList<String>()
             val airports = sortedSetOf<String>()
-            val airportNumberMap = hashMapOf<String,Int>()
-            val airportNameMap = hashMapOf<Int,String>()
+            val nameToNumber = hashMapOf<String,Int>()
+            val numberToName = hashMapOf<Int,String>()
             tickets.forEach { airports.addAll(it)}
             airports.forEachIndexed { index, s ->
-                airportNameMap[index] = s
-                airportNumberMap[s] = index
+                numberToName[index] = s
+                nameToNumber[s] = index
             }
-            val map = Array(airports.size){ArrayList<Int>()}
-            tickets.forEach{(from,to)->
-                map[airportNumberMap[from]!!].add(airportNumberMap[to]!!)
+            val way = Array(airports.size){ArrayList<Int>()}
+            tickets.forEach{(from,to)-> way[nameToNumber[from]!!].add(nameToNumber[to]!!) }
+            way.forEach { it.sort() }
+            val wayUsed = Array(airports.size){BooleanArray(way[it].size){false} }
+            val startPoint = nameToNumber["ICN"]!!
+            var found = false
+            fun dfs(visiting:Int,ticketUsed:Int){
+                if(found) return
+                else if(ticketUsed==tickets.size){
+                    found = true
+                    path.forEach {answer.add(numberToName[it]!!)}
+                    return
+                }
+                for(i in way[visiting].indices){
+                    val next = way[visiting][i]
+                    if(!wayUsed[visiting][i]){
+                        wayUsed[visiting][i] = true
+                        path.add(next)
+                        dfs(next,ticketUsed+1)
+                        path.removeAt(path.lastIndex)
+                        wayUsed[visiting][i] = false
+                    }
+                }
             }
-            return answer
+            path.add(startPoint)
+            dfs(startPoint,0)
+            return answer.toTypedArray()
         }
     }
 }
