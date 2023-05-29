4665064275ce154d2fa72e412ee8fd39b178e3f6
Eunwoo Tae <37829895+kstew16@users.noreply.github.com>
2023-05-29 16:15:44 +0900

    Translated

---
+++ programmers_java/src/다단계칫솔판매.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+class Solution1 {
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        int n = enroll.length;
+        HashMap<String,Integer> nameToNum = new HashMap<String,Integer>();
+        int[] sold = new int[n];
+        int[] parent = new int[n];
+        for(int i=0;i<n;i++){nameToNum.put(enroll[i],i);}
+        for(int i=0;i<n;i++){
+            if(!referral[i].equals("-"))parent[i] = nameToNum.get(referral[i]);
+            else parent[i] = -1;
+        }
+        for(int i=0;i<seller.length;i++){giveFeedback(nameToNum.get(seller[i]),amount[i],sold,parent);}
+        return sold;
+    }
+    void giveFeedback(int me,int amountLeft,int[] sold,int[] parent){
+        int toParent = amountLeft/10;
+        int mine = amountLeft - toParent;
+        sold[me] += mine;
+        if(toParent>0 && parent[me]!=-1) giveFeedback(parent[me],toParent,sold,parent);
+    }
+}
